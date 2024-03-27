4d61480b61cdaf313b0587c10835623047a2a236
Adooz <kndonake@gmail.com>
2024-03-27 18:12:39 +0100

    added even and odd numbers as hint

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let randomNumber = Math.floor(Math.random() * 100) + 1;
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHi = document.querySelector('.lowOrHi');
+const remainingGuesses = document.querySelector('.remainingGuesses');
 
 const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
@@ -38,15 +39,24 @@ function checkGuess() {
         lastResult.textContent = 'Wrong!';
         lastResult.classList.add("wrong");
         if(userGuess < randomNumber) {
-            lowOrHi.textContent = 'Last guess was too low!';
+            if (randomNumber % 2 === 0) {
+                lowOrHi.textContent = 'Last guess was too low and required number is an even number';
+            } else {
+                lowOrHi.textContent = 'Last guess was too low and required number is an odd number';
+            }
         } else if(userGuess > randomNumber) {
-            lowOrHi.textContent = 'Last guess was too high!';
+            if (randomNumber % 2 === 0) {
+                lowOrHi.textContent = 'Last guess was too high and required number is an even number';
+            } else {
+                lowOrHi.textContent = 'Last guess was too high and required number is an odd number';
+            }
         }
     }
 
     guessCount++;
     guessField.value = '';
     guessField.focus();
+    updateRemainingGuesses(); // Update the remaining guesses
 }
 
 guessSubmit.addEventListener('click', checkGuess);
@@ -76,5 +86,11 @@ function resetGame() {
     lastResult.textContent = '';
     lastResult.style.backgroundColor = '';
 
+    remainingGuesses.textContent = 'Remaining guesses: 7'; // Reset remaining guesses
     randomNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+function updateRemainingGuesses() {
+    let remaining = 7 - guessCount + 1; // Plus 1 because guessCount starts at 1
+    remainingGuesses.textContent = 'Remaining guesses: ' + remaining;
 }
